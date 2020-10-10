0bd71f815e86134b948dceb8e9a46a4926e8c995
repl.it user <replituser@example.com>
2020-10-10 16:33:15 +0000

    Basic interation Client -> Server

--- clienteudp.c
+++ clienteudp.c
@@ -5,57 +5,87 @@
 #include <netdb.h>
 #include <stdlib.h>
 #include <strings.h>
+#include <fcntl.h>
 
 #define DATA "Esta eh a mensagem que quero enviar"
 
 struct mensagem {
   int codigo;
+  char message[500];
   char ip[50];
   int porta;
 };
 
-//gcc clienteudp.c -o c && gcc serverudp.c -o s && ./s &
-main()
+//gcc -w clienteudp.c -o c && gcc -w serverudp.c -o s && ./s &
+//pkill s
+
+int main()
 {
-	int sock;
-	struct sockaddr_in name;
+	int sock, op;
+	struct sockaddr_in nameServer, name;
 	struct hostent *hp, *gethostbyname();
   struct mensagem msg;
 
-        /* Cria o socket de comunicacao */
+  /* Cria o socket de comunicacao */
 	sock = socket(AF_INET, SOCK_DGRAM, 0);
 	if(sock<0) {
-	/*
-	/- houve erro na abertura do socket
-	*/
-		perror("opening datagram socket");
+	/* houve erro na abertura do socket*/
+		perror("[Client] Opening datagram socket");
 		exit(1);
 	}
 	/* Associa */
-        hp = gethostbyname("localhost"); //chama dns
-        if (hp==0) {
-            fprintf(stderr, "localhost unknown host ");
-            exit(2);
-        }
-        bcopy ((char *)hp->h_addr, (char *)&name.sin_addr, hp->h_length);
-	name.sin_family = AF_INET;
-	name.sin_port = htons(2020);
+  hp = gethostbyname("localhost"); //chama dns
+  if (hp==0) {
+      fprintf(stderr, "[Client] Localhost unknown host ");
+      exit(2);
+  }
+  /* Já pre define o servidor de acesso */
+  bcopy ((char *)hp->h_addr, (char *)&nameServer.sin_addr, hp->h_length);
+	nameServer.sin_family = AF_INET;
+	nameServer.sin_port = htons(2020);
 
-  msg.codigo=1;
+  while(1){
+   
+    /* Pergunta pro usuario a opcao desejada */
+    printf("\nDigite a opcao desejada:\n");
+    printf("1.Ler; \n");
+    printf("2.Criar;\n");
+    printf("3.Editar;\n");
+    printf("4.Excluir;\n");
+    printf("5.Arquivos Criados;\n");
+    printf("0.Sair.\n\n");
+    printf("Opcao: ");
+
+    scanf("%i", &op);
+    getchar();
+
+    if(op == 0)
+      break;
+
+    msg.codigo = op;
+
+    printf("[Client] Vou enviar em\n");
+    /* Envia */
+    if (sendto (sock, (char *)&msg, sizeof (struct mensagem), 0, (struct sockaddr *) &nameServer, sizeof nameServer) < 0)
+      perror("[Client] Sending datagram message");
+
+    if(msg.codigo == 9){
+      printf("[Client] Codigo para auto destruicao\n");
+      break;
+    }
+
+    /* Recebe a resposta */
+    int resposta, tam;
+    if (recvfrom(sock,(char *)&msg,sizeof(struct mensagem),0,(struct sockaddr *)&name, &tam)<0)
+                    perror("[Client] receiving datagram packet");
+
+    printf("[Client] Porta recebida:  %i\n", ntohs(msg.porta));
+    printf("[Client] IP recebido :    %s\n", msg.ip);
+    printf("[Client] Codigo recebido: %i\n", msg.codigo);
+  }
   
-	/* Envia */
-	if (sendto (sock,(char *)&msg,sizeof (struct mensagem), 0, (struct sockaddr *)&name,
-                    sizeof name)<0)
-                perror("sending datagram message");
+  close(sock);
+  exit(0);
 
-int resposta, tam;
-if (recvfrom(sock,(char *)&msg,sizeof(struct mensagem),0,(struct sockaddr *)&name, &tam)<0)
-                perror("[Client] receiving datagram packet");
-
-printf("[Client] Porta recebida:  %i\n", msg.porta);
-printf("[Client] IP recebido :    %s\n", msg.ip);
-printf("[Client] Codigo recebido: %i\n", msg.codigo);
-        close(sock);
-        exit(0);
 }
 
